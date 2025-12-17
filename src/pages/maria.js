37e51ceaa0387722613f19f7bbbc3b0eed61626e
mariacordeiro-design <maria.cordeiro@tecnico.ulisboa.pt>
2025-12-17 12:34:13 +0000

    Update maria.js

--- src/pages/maria.js
+++ src/pages/maria.js
@@ -2,27 +2,15 @@ import PersonalPage from '../components/about_us/PersonalPage';
 
 const mariaContent = (
         <div class="text-left text-gray-200 space-y-4">
-  <ul class="list-disc list-inside space-y-2">
-    <li>
-      <span class="font-semibold">Nome:</span> Maria Pimentel Cordeiro
-    </li>
-    <li>
-      <span class="font-semibold">Curso:</span> Licenciatura em Engenharia Informática e de Computadores
-    </li>
-    <li>
-      <span class="font-semibold">Hobbies/interesses:</span> Música, Filmes, experimentar coisas novas
-    </li>
-    <li>
-      <span class="font-semibold">Projetos:</span> Projetos académicos no contexto do curso e alguns projetos desenvolvidos para estágios
-    </li>
-    <li>
-      <span class="font-semibold">O que espero aprender:</span> Desenvolver projetos em contexto real, melhorar competências técnicas e comunicação
-    </li>
-    <li>
-      <span class="font-semibold">Como espero contribuir:</span> Responsabilidade, espírito de equipa e vontade constante de aprender
-    </li>
-  </ul>
-</div>
+            <p><strong>Nome:</strong> Maria Pimentel Cordeiro</p>
+            <p><strong>Curso:</strong> Licenciatura em Engenharia Informática e de Computadores</p>
+            <p><strong>Hobbies/interesses:</strong> Música, Filmes, experimentar coisas novas</p>
+            
+            <p><strong>Projetos:</strong> Projetos académicos no contexto do curso e alguns projetos desenvolvidos em estágios</p>
+            <p><strong>O que espero aprender:</strong> Desenvolver projetos em contexto real, melhorar competências técnicas e comunicação</p>
+            <p><strong>Como espero contribuir:</strong> Responsabilidade, espírito de equipa e vontade constante de aprender
+            </p>
+    </div>
 
 );
 
